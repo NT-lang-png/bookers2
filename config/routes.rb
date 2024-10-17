421091086ecde1c887b7b211b93a70885534bc05
Your Name <183378925+NT-lang-png@users.noreply.github.com>
2024-10-17 18:55:33 +0000

    [finish]bookコントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
+  
+  get 'books/new'
+  get 'books/create'
+  get 'books/index'
+  get 'books/show'
+  get 'books/edit'
+  get 'books/update'
+  get 'books/destroy'
   devise_for :users
-
+  root to: 'homes#top'
+  get 'home/about' => 'homes#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
